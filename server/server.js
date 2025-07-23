f3466a2eebe4c6defc10394c787cede5954f9753
alsongard <alsongard@gmail.com>
2025-07-23 21:49:00 +0300

    updating server

--- server/server.js
+++ server/server.js
@@ -284,9 +284,10 @@ app.get("/users", async (req, res)=>{
 const PORT = process.env.PORT_NUMBER;
 console.log(PORT)
 app.listen(PORT, ()=>{console.log(`Listening on port ${PORT}: http://localhost:${PORT}`)})
+const user = process.env.MONGO_USR;
+const password = process.env.MONGO_PASSWD;
 
-
-mongoose.connect("mongodb+srv://alsongadizo:fECAj0gGO7EwoUsk@cluster0.f2dd9sx.mongodb.net/BugDB?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.f2dd9sx.mongodb.net/BugDB?retryWrites=true&w=majority&appName=Cluster0`)
     .then(()=>{
         console.log("Connected to MongoDB");
     })
