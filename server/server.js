e34c408c3b8a9319e58ec573a8b9d8614cc69216
alsongard <alsongard@gmail.com>
2025-07-24 22:05:07 +0300

    used for routes explicitly

--- server/server.js
+++ server/server.js
@@ -7,23 +7,23 @@ const Bug = require("./models/bug.model");
 const mongoose = require("mongoose")
 require("dotenv").config();
 
-const coroptions = {
-    origin:"http://localhost:5173"
-};
+// const coroptions = {
+//     origin:"http://localhost:5173"
+// };
 
 const app = express();
 
-app.use(cors(coroptions));
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+// app.use(cors(coroptions));
 
 const saltRound = 10;
 
 
 app.get("/", (req, res)=>{
-    return res.send("<h1>Welcome Back</h1>")
+    return res.status(200).send("<h1>Welcome Back</h1>")
 })
-// working successfully
+// working successfully: integrated successfully 
 app.post("/bug", async (req,res)=>{
     const {bugname, buglevel,bugStatus, bugDescription, user_id} = req.body;
     try
@@ -43,7 +43,7 @@ app.post("/bug", async (req,res)=>{
         return res.status(400).json({success:true, msg:`Error ${err}`})
     }
 })
-
+// get all bugs: working successfully and integrated completed
 app.get("/bugs", async (req,res)=>{
     try
     {
@@ -55,16 +55,18 @@ app.get("/bugs", async (req,res)=>{
         return res.status(500).json({success:true, msg:err});
     }
 })
-// search for bug based on bug_id
+// search for bug based on bug_id: working successfully integration completed
 app.get("/bug/:id", async (req,res)=>{
     const {id} = req.params;
-    try
+    try //hatua.zetech.ac.ke
     {
         if (!id)
         {
             return res.status(200).json({success:false, msg:'No Id  given'})
         }
-        const bug_found = await Bug.fingById(id);
+        const bug_found = await Bug.findById(id);
+        console.log(`Bug found:`);
+        console.log(bug_found);
         if (!bug_found)
         {
             return res.status(400).json({success:false, msg:`No product with id ${id}`});
@@ -79,33 +81,43 @@ app.get("/bug/:id", async (req,res)=>{
 
     }
 })
-// get bugs based on user_id
+// get bugs based on user_id: working successfully integration completed
 app.get("/getUserId/:id", async (req, res)=>{
+    const {id} = req.params;
+    // console.log(`User ID: ${id}`);
     if (!id)
     {
             
         return res.status(400).json({success:false, msg:'Bad Request'})
     }
+    // check if user exists
+    const user = await User.findById(id);
+    if (!user)
+    {
+        return res.status(400).json({success:false, msg:'No user with the given id'});
+    }
+    // console.log(`User found: ${user.email} with id: ${user._id}`);
     const bugs = await Bug.find({user_id: id});
     if (bugs.length === 0)
     {
-        return res.status(200).json({success:false, msg:'No user with the given id'});
+        return res.status(200).json({success:false, msg:'No bugs found for the given user id'});
     }
-
+    return res.status(200).json({success:true, msg:'Bugs fetched successfully', data:bugs});
 })
 
 
-// update bug based on bug_id
+// update bug based on bug_id : working and integrated successfully
 app.put("/bug/:id", async (req, res)=>{
     const {id} = req.params;
-    const {bugLevel} = req.body;
+    const {bugStatus, bugId} = req.body;
+    console.log(`updatign bug with status ${bugStatus} and id: ${id}`);
     try
     {
         if (!id)
         {
             return res.status(400).json({success:false, msg:`No product with id: ${id}`})
         }
-        const bug_updated = await Bug.findByIdAndUpdate({_id:id}, {bug_level:bugLevel});
+        const bug_updated = await Bug.findByIdAndUpdate({_id:id}, {bug_status:bugStatus});
         const updated_bug = await Bug.findOne({_id:id});
         return res.status(200).json({sucess:true, msg:"Bug Updated", data:updated_bug});
     }
@@ -115,7 +127,7 @@ app.put("/bug/:id", async (req, res)=>{
         return res.status(500).json({success:true, msg:`err`});
     }
 })
-// delete bug based on id
+// delete bug based on id and working successfully
 app.delete("/bug/:id", async (req,res)=>{
     const {id} = req.params;
     console.log(`Deleting bug with id: ${id}`)
@@ -129,6 +141,8 @@ app.delete("/bug/:id", async (req,res)=>{
         console.log(result);
         if (!result) // no id is found by method findByIdAndDelete() returns null
         {
+            console.log(`No product with id: ${id}`);
+            console.log(result);
             return res.status(400).json({success:false, msg:`No product with id : ${id}`})
         }
         return res.status(200).json({succes:true, msg:'Successfully deleted product'});
@@ -141,9 +155,11 @@ app.delete("/bug/:id", async (req,res)=>{
 })
 
 
-// register user
+// register user: working and integrated successfully
 app.post("/register", async (req,res)=>{
-    const {userEmail, userPassword} = req.body;
+    const {useremail, password} = req.body;
+    const userEmail = useremail;
+    const userPassword = password;
     try
     {
         console.log(`UserEmail: ${userEmail}`);
@@ -166,19 +182,30 @@ app.post("/register", async (req,res)=>{
 })
 
 
-// login user
+// login user: working successfully and integrated successfully
 app.post("/login", async (req,res)=>{
     const {useremail, password} = req.body;
+
     try
     {
         const foundUser = await User.findOne({email:useremail});
         if(foundUser)
         {
-            
+            console.log(`User found`);
+            console.log(foundUser);
+            console.log(foundUser.email)
+
             const result = await bcrypt.compare(password, foundUser.password);
             if (result)
             {
-                return res.status(201).json({success:true, msg:"Login successfully", data:{useremail:foundUser.useremail, role:foundUser.role, user_id:foundUser._id}})
+                const userObject = {user_id: foundUser._id}
+                jwt.sign(userObject, process.env.JWT_SECRET, {expiresIn: '1h'}, (err, token) => {
+                    if (err) {
+                        console.error("Error signing JWT:", err);
+                        return res.status(500).json({success:false, msg:"Error signing JWT"});
+                    }
+                    return res.status(200).json({success:true, msg:"Login successfully", data:{useremail:foundUser.email, role:foundUser.role, user_id:foundUser._id, token:token}});
+                });
             }
             else
             {
@@ -198,13 +225,20 @@ app.post("/login", async (req,res)=>{
 })
 
 
-// create admin
+// create admin : working and integrated successfully
 app.post("/admin", async (req,res)=>{
-    const {role, useremail, userPasswd} = req.body;
-
+    const {role, email, password} = req.body;
+    console.log(` {req.body}: role: ${role} \n email: ${email} \n password: ${password}`);
+    const useremail = email;
+    const userPasswd = password;
     try
     {
-        console.log(`role: ${role} \n useremail : ${useremail} \n userPasswd: ${userPasswd}`)
+        console.log(`role: ${role} \n useremail : ${useremail} \n userPasswd: ${userPasswd}`);
+        if (!role || !useremail || !userPasswd)
+        {
+            console.log(`Received empty value`);
+            return res.status(400).json({success:false, msg:"N"})
+        }
         const existingAdmin =  User.find({email:useremail});// returns an array
         console.log(existingAdmin.length);
         if (existingAdmin.length === 0)
@@ -224,11 +258,12 @@ app.post("/admin", async (req,res)=>{
     }
 })
 
-//login admin
+//login admin: : working and integrated successfully
 app.post("/logadmin", async (req, res)=>{
     const {role, useremail, password} = req.body;
     try
     {
+        console.log(`Role :${role}, useremail:${useremail},password: ${password}`)
         if (!role || !useremail || !password)
         {
             console.log(`Received emptyvalue`);
@@ -245,7 +280,7 @@ app.post("/logadmin", async (req, res)=>{
                 const result = await bcrypt.compare(password, foundUser.password);
                 if (result)
                 {
-                    return res.status(201).json({success:true, msg:"Admin Login successfully", data:{useremail:foundUser.useremail, role:foundUser.role, user_id:foundUser._id, user_email:foundUser.email}})
+                    return res.status(200).json({success:true, msg:"Admin Login successfully", data:{useremail:foundUser.useremail, role:foundUser.role, user_id:foundUser._id, user_email:foundUser.email}})
                 }
                 else
                 {
@@ -262,7 +297,7 @@ app.post("/logadmin", async (req, res)=>{
     }
 })
 // # ADMIN FUNCTIONALAITIES
-// get all users
+// get all users : working and integrated successfully
 app.get("/users", async (req, res)=>{
     try
     {
@@ -283,14 +318,16 @@ app.get("/users", async (req, res)=>{
 
 const PORT = process.env.PORT_NUMBER;
 console.log(PORT)
-app.listen(PORT, ()=>{console.log(`Listening on port ${PORT}: http://localhost:${PORT}`)})
 const user = process.env.MONGO_USR;
 const password = process.env.MONGO_PASSWD;
 
-mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.f2dd9sx.mongodb.net/BugDB?retryWrites=true&w=majority&appName=Cluster0`)
-    .then(()=>{
-        console.log("Connected to MongoDB");
-    })
-    .catch(err=>{
-        console.log(`Error: ${err}`);
-    })
+// mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.f2dd9sx.mongodb.net/BugDB?retryWrites=true&w=majority&appName=Cluster0`)
+// .then(()=>{
+//         app.listen(PORT, ()=>{console.log(`Listening on port ${PORT}: http://localhost:${PORT}`)})
+//         console.log("Connected to MongoDB");
+//     })
+//     .catch(err=>{
+//         console.log(`Error: ${err}`);
+//     })
+
+module.exports = app;
